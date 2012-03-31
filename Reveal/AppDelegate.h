787137283e29c7bd43956a7bf9cd82b7c7e25fb3
Patrick McConnell <patrick@dogboystudios.com>
2012-03-31 10:56:02 -0400

    Initial Commit

---
+++ Reveal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Reveal
+//
+//  Created by Patrick McConnell on 3/31/12.
+//  Copyright (c) 2012 Dogboy Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Reveal/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Reveal
+//
+//  Created by Patrick McConnell on 3/31/12.
+//  Copyright (c) 2012 Dogboy Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
